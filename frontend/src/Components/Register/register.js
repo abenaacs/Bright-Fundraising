6a63f6dbf18ded7f1ef00eb7ef2b5d495323461c
Yared3214 <yaredman0099@gmail.com>
2024-07-31 02:30:35 -0700

    Register Page updated

--- frontend/src/Components/Register/register.js
+++ frontend/src/Components/Register/register.js
@@ -1,154 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import signin from '../../assets/signup.png'
-import { Link, useSearchParams } from 'react-router-dom';
-import countries from './country';
-
-const Step1 = ({ formData, setFormData }) => (
-    <div className='flex flex-col md:gap-5 gap-3 items-center'>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Full Name</h4></label>
-            <input value={formData.fullName}
-                onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
-                type='text'
-                required
-                className='bg-[#d9e1ff] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5 md:w-[580px]' />
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Email</h4></label>
-            <input value={formData.email}
-                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                type='email'
-                required
-                className='bg-[#d9e1ff] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5 md:w-[580px]' />
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Phone</h4></label>
-            <input value={formData.phone}
-                onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
-                type='number'
-                required
-                className='bg-[#d9e1ff] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5 md:w-[580px]' />
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Location</h4></label>
-            <select value={formData.location}
-                onChange={(e) => setFormData({ ...formData, location: e.target.value })}
-                className='bg-[#d9e1ff] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5 md:w-[580px]'>
-                {countries.map((country, index) => (
-                    <option key={index}><img src={country.flag} width={50} />{country.name}</option>
-                ))}
-            </select>
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Password</h4></label>
-            <input value={formData.password}
-                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-                type='password'
-                required
-                className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5' />
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Confirm Password</h4></label>
-            <input type='password' required className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md h-[28px] md:h-[32px] text-black px-5' />
-        </div>
-    </div>
-);
-
-const Step2 = ({ formData, setFormData }) => (
-    <div className='flex flex-col md:gap-5 gap-3 items-center'>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Startup Name</h4></label>
-            <input value={formData.startUpName}
-                onChange={(e) => setFormData({ ...formData, fundRaiser: { ...formData.fundRaiser, startUpName: e.target.value } })}
-                type='text'
-                required
-                className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5' />
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Business Sector</h4></label>
-            <select value={formData.businessSector}
-                onChange={(e) => setFormData({ ...formData, fundRaiser: { ...formData.fundRaiser, businessSector: e.target.value } })}
-                className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5'>
-                <option>Tech</option>
-                <option>Agriculture</option>
-                <option>Garment</option>
-            </select>
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Business Stage</h4></label>
-            <select value={formData.businessStage}
-                onChange={(e) => setFormData({ ...formData, fundRaiser: { ...formData.fundRaiser, businessStage: e.target.value } })}
-                className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5'>
-                <option>Idea</option>
-                <option>Prototype</option>
-                <option>Early-Stage</option>
-                <option>Growht-Stage</option>
-            </select>
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Business Description</h4></label>
-            <textarea value={formData.businessDesc}
-                onChange={(e) => setFormData({ ...formData, fundRaiser: { ...formData.fundRaiser, businessDesc: e.target.value } })}
-                required
-                rows={4}
-                className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md md:h-[32px] text-black px-5' />
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Business Website</h4></label>
-            <input value={formData.businessWebsite}
-                onChange={(e) => setFormData({ ...formData, fundRaiser: { ...formData.fundRaiser, businessWebsited: e.target.value } })}
-                type='text'
-                required
-                className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5' />
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Pitch Deck Url</h4></label>
-            <input value={formData.fundRaiser.pitchDeck}
-                onChange={(e) => setFormData({ ...formData, fundRaiser: { ...formData.fundRaiser, pitchDeck: e.target.value } })}
-
-
-                type='text'
-                required
-                className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5' />
-        </div>
-    </div>
-)
-
-const Step2_ = ({ formData, setFormData }) => (
-    <div className='flex flex-col md:gap-5 gap-3 items-center'>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Investment Sector</h4></label>
-            <select value={formData.investor.investSector}
-                onChange={(e) => setFormData({ ...formData, investor: { ...formData.investor, investSector: e.target.value } })}
-                className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md md:h-[32px] h-[28px] text-black px-5'>
-                <option>Tech</option>
-                <option>Agriculture</option>
-                <option>Garment</option>
-            </select>
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Investment Experience</h4></label>
-            <textarea value={formData.investor.investExep}
-                onChange={(e) => setFormData({ ...formData, investor: { ...formData.investor, investExep: e.target.value } })}
-                required
-                rows={4}
-                className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md md:h-[32px] text-black px-5' />
-        </div>
-        <div className='flex flex-col gap-0'>
-            <label><h4 className='text-lg md:text-xl'>Linked Profile</h4></label>
-            <input value={formData.investor.linkedIn}
-                onChange={(e) => setFormData({ ...formData, investor: { ...formData.investor, linkedIn: e.target.value } })}
-                type='text'
-                required
-                className='bg-[#d9e1ff] md:w-[580px] w-[300px] rounded-md md:h-[32px] text-black px-5' />
-        </div>
-    </div>
-)
+import React, { useState } from 'react'
+import { Link, useRoutes, useSearchParams } from 'react-router-dom';
+import RegisterCommon from './RegisterCommon';
+import RegisterFundraiser from './RegisterFundraiser';
+import RegisterInvestor from './RegisterInvestor';
+import useRegister from '../../hooks/useRegister';
 
 function Register() {
     const [searchParams] = useSearchParams();
     const role = searchParams.get('role');
     const [step, setStep] = useState(1);
+    const {register, error, loading} = useRegister(); 
     const [formData, setFormData] = useState({
         fullName: '',
         email: '',
@@ -181,27 +42,21 @@ function Register() {
         setStep((prevStep) => prevStep - 1);
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formData);
+    const handleSubmit = async(e) => {
+        await register(formData);
+        // e.preventDefault();
+        // console.log(formData);
     };
 
     return (
-        <div className='md:grid md:grid-cols-2 md:gap-5 flex h-screen justify-center items-center'>
-            <div className='pr-10 hidden md:flex md:justify-center md:mt-10'>
-                <img src={signin} width={400} className='h-[600px]' />
-            </div>
-            <div>
-                <div className='flex flex-col md:mt-0 gap-5 md:gap-10 md:p-10 items-center mt-14'>
-                    <div className='flex flex-col items-center'>
-                        <h2 className='text-white text-center text-3xl md:text-5xl md:mb-5'>Sign Up</h2>
-                        <h6 className='text-center text-sm md:text-md'>Enter Your info to proceed</h6>
-                    </div>
-                    <form className='flex flex-col items-start'>
+                    <form onSubmit={handleSubmit} className='flex flex-col items-start'>
                         <div className='flex flex-col gap-5'>
-                            {step === 1 && <Step1 formData={formData} setFormData={setFormData} />}
-                            {step === 2 && role === 'fundRaiser' && <Step2 formData={formData} setFormData={setFormData} />}
-                            {step === 2 && role === 'investor' && <Step2_ formData={formData} setFormData={setFormData} />}
+                            {/* Common Register fields for both investor and fundRaiser */}
+                            {step === 1 && <RegisterCommon formData={formData} setFormData={setFormData} />}
+                            {/* Register fields only for the fundRaiser  */}
+                            {step === 2 && role === 'fundRaiser' && <RegisterFundraiser formData={formData} setFormData={setFormData} />}
+                            {/* Register fields only for the investor */}
+                            {step === 2 && role === 'investor' && <RegisterInvestor formData={formData} setFormData={setFormData} />}
                             <div>
                                 {step < 2 && <button className='md:w-[580px] w-[300px] mx-2' type="button" onClick={nextStep}>Next</button>}
                                 <div className='flex justify-between'>
@@ -217,9 +72,6 @@ function Register() {
                             </div>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>
     )
 }
 
