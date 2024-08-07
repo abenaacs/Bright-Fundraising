88bcb64d15557253b71068a44ba8f04244c601cd
Yared3214 <yaredman0099@gmail.com>
2024-08-07 01:49:50 -0700

    regiser page Updated

--- frontend/src/Components/Register/register.js
+++ frontend/src/Components/Register/register.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import { Link, useSearchParams } from 'react-router-dom';
 import RegisterCommon from './RegisterCommon';
-import RegisterFundraiser from './RegisterFundraiser';
-import RegisterInvestor from './RegisterInvestor';
+import FundraiserCompanyInfo from './FundraiserCompanyInfo';
+import Investor from './investor';
 import useRegister from '../../hooks/useRegister';
+import FundraiserSocialInfo from './FundraiserSocialInfo';
 
 function Register() {
     const [searchParams] = useSearchParams();
@@ -17,15 +18,24 @@ function Register() {
         phone: null,
         location: '',
         password: '',
+        altPhone: null,
+        birthdate: '',
         address: '',
         city: '',
         fundRaiser: {
             startUpName: '',
             businessSector: '',
             businessStage: '',
+            incorporationDate: '',
             businessDesc: '',
+            tinNumber: '',
+            license: '',
+            id: '',
             businessWebsite: '',
-            pitchDeck: ''
+            telegram: '',
+            whatsapp: '',
+            linkedin: '',
+            instagram: '',
         },
         investor: {
             investSector: '',
@@ -37,6 +47,7 @@ function Register() {
     });
     const nextStep = () => {
         setStep((prevStep) => prevStep + 1);
+        console.log(formData)
     };
 
     const prevStep = () => {
@@ -45,8 +56,6 @@ function Register() {
 
     const handleSubmit = async (e) => {
         await register(formData);
-        // e.preventDefault();
-        // console.log(formData);
     };
 
     return (
@@ -54,15 +63,33 @@ function Register() {
             <div className='flex flex-col gap-5'>
                 {/* Common Register fields for both investor and fundRaiser */}
                 {step === 1 && <RegisterCommon formData={formData} setFormData={setFormData} />}
-                {/* Register fields only for the fundRaiser  */}
-                {step === 2 && role === 'fundRaiser' && <RegisterFundraiser formData={formData} setFormData={setFormData} />}
+                {/* Register fields only for the fundRaiser */}
+                {/* fields for the company info */}
+                {step === 2 && role === 'fundRaiser' && <FundraiserCompanyInfo formData={formData} setFormData={setFormData} />}
+                {/* fields for the company social media info */}
+                {step === 3 && role === 'fundRaiser' && <FundraiserSocialInfo formData={formData} setFormData={setFormData} />}
                 {/* Register fields only for the investor */}
-                {step === 2 && role === 'investor' && <RegisterInvestor formData={formData} setFormData={setFormData} />}
+                {step === 2 && role === 'investor' && <Investor formData={formData} setFormData={setFormData} />}
+
                 <div>
-                    {step < 2 && <button className='md:w-[580px] w-[300px] mx-2' type="button" onClick={nextStep}>Next</button>}
+                    {step < 2 && role === 'investor' &&
+                        <button className='md:w-[580px] w-[300px] mx-2' type="button"
+                            onClick={nextStep}>Next</button>}
                     <div className='flex justify-between'>
-                        {step > 1 && <button className='md:w-[150px] w-[100px] text-lg md:text-xl' type="button" onClick={prevStep}>Previous</button>}
-                        {step === 2 && <button className='md:w-[150px] w-[100px] text-lg md:text-xl' type="submit">Submit</button>}</div>
+                        {step > 1 &&
+                            <button className='md:w-[150px] w-[100px] text-lg md:text-xl' type="button"
+                                onClick={prevStep}>Previous</button>}
+                        {step < 3 && role === 'fundRaiser' &&
+                            <button className={`${step === 2 ? 'md:w-[150px] w-[100px] text-lg md:text-xl' : 'md:w-[580px] w-[300px] mx-2'}`}
+                                type="button"
+                                onClick={nextStep}>Next</button>}
+                        {step === 3 && role === "fundRaiser" &&
+                            <button className='md:w-[150px] w-[100px] text-lg md:text-xl'
+                                type="submit">Submit</button>}
+                        {step === 2 && role === "investor" &&
+                            <button className='md:w-[150px] w-[100px] text-lg md:text-xl'
+                                type="submit">Submit</button>}
+                    </div>
                     <div className='flex md:flex-row flex-col md:justify-between items-center gap-2 mt-3 md:mt-0'>
                         <Link to='/forgot'><h6 className='text-white font-bold text-[15px]'>Forgot Password?</h6></Link>
                         <div className='flex'>
